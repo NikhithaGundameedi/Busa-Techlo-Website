b43edd61fe98aba67c69c6dfee1b8bb78f551ef6
Nikhitha <116160147+NikhithaGundameedi@users.noreply.github.com>
2023-06-28 12:04:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function ScrollIntocontact(){
+    const element=document.querySelector('.scrolltocontact');
+    element.scrollIntoView({ behavior: "smooth" });
+}
+function ScrollIntoservices(){
+    const element=document.querySelector('.scrolltoservices');
+    element.scrollIntoView({ behavior: "smooth" });
+}
+
+function ScrollIntohome(){
+    const element=document.querySelector('.scrolltohome');
+    element.scrollIntoView({ behavior: "smooth" });
+}
+function ScrollIntoabout(){
+    const element=document.querySelector('.scrolltoabout');
+    element.scrollIntoView({ behavior: "smooth" });
+}
+function ScrollIntoteam(){
+    const element=document.querySelector('.scrolltoteam');
+    element.scrollIntoView({ behavior: "smooth" });
+}
